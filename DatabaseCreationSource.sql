707e3de7fefeb0690927710bf2b667b5561f21c6
neilrls <n.rafferty@kainos.com>
2015-07-07 10:48:41 +0100

    Updated library db to contain more available books

--- DatabaseCreationSource.sql
+++ DatabaseCreationSource.sql
@@ -9,9 +9,9 @@ Create user 'libraryUser'@localhost identified by 'MySuperSafePassw0rd!';
 Grant Select, Update, Delete on Book to 'libraryUser'@'localhost';
 
 Insert into Book(title, author, yearPublished, category, isAvailable) values('Linux Complete', 'Grant Taylor', 1999, 'Technical', 0);
-Insert into Book(title, author, yearPublished, category, isAvailable) values('Pandora\'s Box', 'Andrew A. Adams & Rachel J. McCrindle', 1969, 'Delivery - Project Management', 0);
-Insert into Book(title, author, yearPublished, category, isAvailable) values('The Project Management', 'Richard Newton', 1964, 'Technical - Project Management', 0); 
-Insert into Book(title, author, yearPublished, category, isAvailable) values('Refractoring Databases', 'Scott W.Ambler & Pramod J. Sadalage', 2006, 'Technical - Database Administration', 0);
+Insert into Book(title, author, yearPublished, category, isAvailable) values('Pandora\'s Box', 'Andrew A. Adams & Rachel J. McCrindle', 1969, 'Delivery - Project Management', 1);
+Insert into Book(title, author, yearPublished, category, isAvailable) values('The Project Management', 'Richard Newton', 1964, 'Technical - Project Management', 1); 
+Insert into Book(title, author, yearPublished, category, isAvailable) values('Refractoring Databases', 'Scott W.Ambler & Pramod J. Sadalage', 2006, 'Technical - Database Administration', 1);
 Insert into Book(title, author, yearPublished, category, isAvailable) values('TCP/IP Network Administration', 'Craig Hunt', 1994, 'Technical - Networking', 0);
 Insert into Book(title, author, yearPublished, category, isAvailable) values('TCP/IP Network Administration', 'Craig Hunt', 1994, 'Technical -Networking', 1);
 
