5510f839b7407c726e04974979af0dbc8198fa31
Andrew Speed <A.Speed@kainos.com>
2015-07-07 11:10:42 +0100

    Refactored project structure to enable jUnit testing.

---
+++ src/com/kainos/librarysystem/resource/ViewsResourceTest.java
@@ -0,0 +1,14 @@
+package com.kainos.librarysystem.resource;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ViewsResourceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
